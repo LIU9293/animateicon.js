ae5b62a84132bef73380bc139c083070379bcc90
lock <lock@lockdeMacBook-Pro.local>
2016-10-18 15:40:05 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 var star = `<img id="animate-star" style="position: absolute;" src="https://oa932w8s4.qnssl.com/simple_collect.png"/>`;
 
+var APIurl = 'https://api.github.com/repos/liu9293/animateicon.js/contents/icons';
+var images = {};
+
+var req = new XMLHttpRequest();
+
+function stateChange(){
+  if (req.readyState==4){// 4 = "loaded"
+    if (req.status==200){// 200 = "OK"
+      var res = JSON.parse(req.responseText);
+      for(var i = 0; i < res.length; i++){
+        images[res[i].name] = res[i].download_url
+      };
+    }
+    else{
+      console.log(req.statusText);
+    }
+  }
+}
+req.onreadystatechange = stateChange();
+req.open("GET", APIurl, true);
+req.send();
+
+
 //Animate() will be a function for any DOM element
 Object.prototype.Animate = function(options){
 
