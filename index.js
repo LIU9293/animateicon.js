d10dea8f4c80d751291704236dfc8694187d7926
careerfore <careerfore@careerforedeMac-mini.local>
2016-10-18 16:27:50 +0800

    add

--- index.js
+++ index.js
@@ -12,20 +12,27 @@ function stateChange(){
       for(var i = 0; i < res.length; i++){
         images[res[i].name] = res[i].download_url
       };
+      console.log(images)
     }
     else{
       console.log(req.statusText);
     }
   }
 }
-req.onreadystatechange = stateChange();
+req.onreadystatechange = stateChange;
 req.open("GET", APIurl, true);
 req.send();
 
 
 //Animate() will be a function for any DOM element
 Object.prototype.Animate = function(options){
-
+  while (true) {
+    if(images == {}){
+      continue;
+    }else {
+      break;
+    }
+  }
   //setup initial options
   var initalOptions = {
     type: 'star',
@@ -43,9 +50,10 @@ Object.prototype.Animate = function(options){
     onPressed: options.onPressed || initalOptions.onPressed,
     onReleased: options.onReleased || initalOptions.onReleased
   }
-
+  var that = this;
   //setup motions
   var motion = function(){
+    that.removeEventListener('click', motion);
     Opt.onPressed();
     Icon.style.left = '0px';
     var t = setInterval(function(){
@@ -53,19 +61,18 @@ Object.prototype.Animate = function(options){
       if(left > -3900){
         Icon.style.left = (left - 100) + 'px';
       } else {
+        that.addEventListener('click', cancel);
         clearInterval(t);
       }
-    },30);
-    this.removeEventListener('click', motion);
-    this.addEventListener('click', cancel);
-  }.bind(this);
+    },20);
+  };
 
   var cancel = function(){
     Opt.onReleased();
     Icon.style.left = '0px';
-    this.removeEventListener('click', cancel);
-    this.addEventListener('click', motion);
-  }.bind(this);
+    that.removeEventListener('click', cancel);
+    that.addEventListener('click', motion);
+  };
 
   this.addEventListener('click', motion);
 
